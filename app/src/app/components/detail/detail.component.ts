ebe8bae11988fb5dcbe0f4edf6a6726f9a41fcf8
Carlos Ferrer <cafema01@floridauniversitaria.es>
2024-05-05 11:41:35 +0200

    Insertamos comentarios en bbdd

--- app/src/app/components/detail/detail.component.ts
+++ app/src/app/components/detail/detail.component.ts
@@ -2,18 +2,32 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MediaService } from '../../core/services/media.service';
 import { Movies, Serie } from '../../interfaces/media.interface';
-
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { jwtDecode } from 'jwt-decode';
+import { CommentsService } from '../../core/services/comments.service';
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss',
 })
 export class DetailComponent {
   public media!: Movies | Serie;
+  public show: boolean = false;
+  /* public gmail: string | null = this.getUserName(); */
 
-  constructor(private route: ActivatedRoute, private mediaSvc: MediaService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private mediaSvc: MediaService,
+    private comentarioSvc: CommentsService
+  ) {}
+
+  comentariosForm = new FormGroup({
+    comentario: new FormControl(),
+    gmail: new FormControl(),
+    idMedia: new FormControl(),
+  });
 
   ngOnInit(): void {
     // Recuperamos la id de la url
@@ -21,8 +35,30 @@ export class DetailComponent {
     if (id !== null) {
       this.mediaSvc.getMediaById(Number(id)).subscribe((item) => {
         this.media = item;
-        console.log(this.media);
       });
+    }
+
+    this.comentariosForm.controls['gmail'].setValue(this.getUserName());
+    this.comentariosForm.controls['idMedia'].setValue(id);
+
+    this.getUserName();
+  }
+
+  onSubmit() {
+    let body = this.comentariosForm.value;
+
+    this.comentarioSvc.insertComment(body);
+    this.comentariosForm.get('comentario')?.reset();
+  }
+
+  getUserName() {
+    let token = localStorage.getItem('token');
+    if (token) {
+      let decodedToken = jwtDecode<{ username: string }>(token);
+      let username = decodedToken.username;
+      return username;
+    } else {
+      return null;
     }
   }
 }
---
+++ app/src/app/core/services/comments.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentsService {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
+
+  insertComment(body: any) {
+    console.log('Cuerpo que le pasamos a la API:', JSON.stringify(body));
+
+    return this.http.post('http://localhost:8000/comentarios', body).subscribe(
+      (res: any) => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Éxito',
+          detail: 'Comentario insertado con éxito',
+        });
+      },
+      (error) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error de inserción',
+          detail: 'Error al insertar usuario',
+        });
+      }
+    );
+  }
+}
