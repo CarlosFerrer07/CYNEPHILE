55e6fd4e2b74c9811fd6287b7faf2c4186ea2bad
Carlos Ferrer <cafema01@floridauniversitaria.es>
2024-05-10 14:45:50 +0200

    Añadimos icono logout y saludo de Bienvenida

--- app/src/app/components/header/header.component.ts
+++ app/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +12,25 @@ import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 export class HeaderComponent {
   constructor(private router: Router) {}
 
+  public userName!: string;
+
+  ngOnInit() {
+    this.getUserName();
+  }
+
   logOut() {
     localStorage.removeItem('token');
 
     this.router.navigateByUrl('/auth');
+  }
+
+  getUserName() {
+    let token = localStorage.getItem('token');
+    if (token) {
+      let decodedToken = jwtDecode<{ username: string }>(token);
+      let username = decodedToken.username;
+      username = username.split('@')[0].toLocaleUpperCase();
+      this.userName = 'Hola ' + username;
+    }
   }
 }
